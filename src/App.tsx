ce39653b5cae1c67a0fedf56b70382a652f0dd53
aktherhossen01 <mdakterhossen122@gmail.com>
2025-02-18 22:15:11 +0600

    handle increment decrement

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
 
 
 function App() {
-  
+  const dispatch = useDispatch();
+
+  const handleIncrement = ()=>{
+    dispatch(increment())
+  }
+  const handledecrement = ()=>{
+    dispatch(decrement())
+  }
 
   return (
    <div>
     <div>
       <h1>calculate</h1>
-      <button>increment</button>
+      <button onClick={handleIncrement}>increment</button>
       <div>0</div>
-      <button>decrement</button>
+      <button onClick={handledecrement}>decrement</button>
     </div>
    </div>
   )
