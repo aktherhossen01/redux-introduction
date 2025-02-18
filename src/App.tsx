7b2bfcec7d608a18b9079b31c730bff8cde2f229
aktherhossen01 <mdakterhossen122@gmail.com>
2025-02-19 02:59:12 +0600

    test smiple project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useDispatch } from "react-redux";
 import { decrement, increment } from "./redux/features/counter/counterSlice";
 
+import { useAppSelector } from "./redux/hook";
+
 
 function App() {
   const dispatch = useDispatch();
+  const {count} = useAppSelector((state)=>state.counter);
+  
+  
 
-  const handleIncrement = ()=>{
-    dispatch(increment())
+  const handleIncrement = (ammount:number)=>{
+    dispatch(increment(ammount))
   }
   const handledecrement = ()=>{
     dispatch(decrement())
@@ -16,8 +21,9 @@ function App() {
    <div>
     <div>
       <h1>calculate</h1>
-      <button onClick={handleIncrement}>increment</button>
-      <div>0</div>
+      <button onClick={ ()=>handleIncrement(7)}>increment by 7</button>
+      <button onClick={()=>handleIncrement(1)}>increment</button>
+      <div>{count}</div>
       <button onClick={handledecrement}>decrement</button>
     </div>
    </div>
---
+++ src/redux/middlewares/logger.ts
@@ -0,0 +1,8 @@
+const logger = (state)=>(next)=>(action)=>{
+    console.log(state.getState());
+    console.log(action);
+    return next(action)
+    
+}
+
+export default logger
